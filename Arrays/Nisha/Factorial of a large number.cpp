9fa0db21f913c1d6015e22663d48740db98f68ae
Ampolu Nisa <63179438+Nisha333-33@users.noreply.github.com>
2021-02-10 17:17:19 +0530

    Update Factorial of a large number.cpp

--- Arrays/Nisha/Factorial of a large number.cpp
+++ Arrays/Nisha/Factorial of a large number.cpp
@@ -1,20 +1,15 @@
 ﻿#include<iostream> 
 using namespace std;
-
 #define MAX 500 
-
 int multiply(int x, int res[], int res_size);
  
 void factorial(int n)
 {
     int res[MAX];
-
     res[0] = 1;
     int res_size = 1;
-
     for (int x = 2; x <= n; x++)
         res_size = multiply(x, res, res_size);
-
     cout << "Factorial of given number is \n";
     for (int i = res_size - 1; i >= 0; i--)
         cout << res[i];
@@ -23,7 +18,6 @@ void factorial(int n)
 int multiply(int x, int res[], int res_size)
 {
     int carry = 0;  
-
     for (int i = 0; i < res_size; i++)
     {
         int prod = res[i] * x + carry;
@@ -32,7 +26,6 @@ int multiply(int x, int res[], int res_size)
 
         carry = prod / 10;
     }
-
     while (carry)
     {
         res[res_size] = carry % 10;
@@ -41,7 +34,6 @@ int multiply(int x, int res[], int res_size)
     }
     return res_size;
 }
-
 int main()
 {
     factorial(100);
